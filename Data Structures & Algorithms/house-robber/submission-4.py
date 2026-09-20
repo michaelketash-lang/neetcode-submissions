844f3795638b254095c5c0a64e73287e832e6e27
Michael Ketash <michael.ketash@gmail.com>
2026-09-20 15:28:43 +0300

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        mem = {}
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            
+            if i == 1:
+                return max(nums[0],nums[1])
+            
+            if i in mem:
+                return mem[i]
+            
+            mem[i] = max(dp(i-2) + nums[i] , dp(i - 1))
+            return mem[i]
+        
+        return dp(len(nums) - 1)
